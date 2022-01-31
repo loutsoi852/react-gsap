242923714010592637d778ad56ab83a8f46f0356
loo.tsoi <loutsoi852@gmail.com>
2022-02-01 01:05:22 +0800

    menu scroll control

--- gsap-app/src/index.js
+++ gsap-app/src/index.js
@@ -8,7 +8,8 @@ import ReactDOM from 'react-dom';
 // import App5 from './App5';
 // import App6 from './App6';
 // import App7 from './App7';
-import App8 from './App8';
+// import App8 from './App8';
+import App9 from './App9';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -19,7 +20,8 @@ ReactDOM.render(
     {/* <App5 /> */}
     {/* <App6 /> */}
     {/* <App7 /> */}
-    <App8 />
+    {/* <App8 /> */}
+    <App9 />
   </React.StrictMode>,
   document.getElementById('root')
 );
